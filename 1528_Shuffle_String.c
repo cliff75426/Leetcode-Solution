0f3f44b75f5e791179b5e4cbd8ac3e4c7e96da32
cliff75426 <cliffchen75426@gmail.com>
2020-08-28 22:50:34 +0800

    #1528 補充邏輯錯誤

--- 1528_Shuffle_String.c
+++ 1528_Shuffle_String.c
@@ -2,8 +2,8 @@ char * restoreString(char * s, int* indices, int indicesSize){
     char *shuffleStr = (char*)malloc(sizeof(char)*(indicesSize+1));
     int k =0;
     for(int i = 0;i<indicesSize;i++){
-        shuffleStr[indices[i]] = s[i];  //shuffleStr[i] = s[indices[i]]; try example get wrong answer.
-        //printf("%c %d\n",shuffleStr[i], i);
+        shuffleStr[indices[i]] = s[i];  
+		//shuffleStr[i] = s[indices[i]]; try example get wrong answer. 邏輯錯誤
         k++;
     }
     shuffleStr[k] = 0;
